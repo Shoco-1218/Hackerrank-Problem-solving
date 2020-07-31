d95f51f9ef665f12b2b4fa42596509ccc2554b1c
Shoko Yanagibashi <shoko.y1218@gmail.com>
2020-07-31 22:37:59 +1000

    Solve problems

---
+++ Migratory-birds.js
@@ -0,0 +1,24 @@
+//Migratory birds 
+function migratoryBirds(arr) {
+  const countN = [0, 0, 0, 0, 0];
+
+  for (let i = 0; i < arr.length; i++){
+      countN[arr[i] - 1 ]++;
+  }
+
+  let largeNum = countN[0];
+  
+  for (let i = 0; i < countN.length; i++){
+      if(countN[i] > largeNum){
+          largeNum = countN[i];
+          console.log("largeNum: " + largeNum);
+      }
+  }
+
+  const index = countN.findIndex(e => e === largeNum);
+
+  let answer = index + 1;
+
+  return answer;
+  
+}
